44ce245def4cfc39cfcaf14945ddcda478147067
algut <agutim07@estudiantes.unileon.es>
2022-07-05 14:03:20 +0200

    dia 2

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
+import ExpandCircle from '@mui/icons-material/ExpandCircleDown';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
@@ -11,6 +12,16 @@ import Grid from '@mui/material/Grid'
 
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import Drawer from '@mui/material/Drawer';
+import { Divider } from '@mui/material';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import Collapse from '@mui/material/Collapse';
+
+import {secciones} from '../data.js';
 
 const theme = createTheme({
     typography: {
@@ -20,32 +31,45 @@ const theme = createTheme({
 },});
 
 const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
 
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  }));
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
 const Header = () => {
   let logoWidth = 80;
-  const drawerWidth = 240;
+  const drawerWidth = 300;
 
-  const [open, setOpen] = React.useState(false);
+  const [openDR, setOpenDR] = React.useState(false);
+  const handleDrawerOpen = () => { setOpenDR(true); };
+  const handleDrawerClose = () => { setOpenDR(false); };
 
-  const handleDrawerOpen = () => { setOpen(true); };
+  const [openMenu, setOpenMenu] = React.useState([]);
+  for(let i=0; i<secciones.length && openMenu.length<secciones.length; i++){
+    openMenu.push(false);
+  }
 
-  const handleDrawerClose = () => { setOpen(false); };
+  function handleClickMenu(num){ 
+    let newMenu = openMenu;
+    if(!newMenu[num]){
+      newMenu[num] = true;
+    }else{
+      newMenu[num] = false;
+    }
+    setOpenMenu([...newMenu]);
+  }
 
   return (
     <div>
       <Toolbar sx={{borderBottom: `1px solid ${theme.palette.grey[300]}`}}>
-        <Grid container alignItems="center">
+        <Grid mb={1.5} container alignItems="center">
           <Grid item xs={3} align="left">
             <IconButton
                 color="inherit" onClick={handleDrawerOpen} edge="start"
-                sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                sx={{ mr: 2 , ...(openDR && { display: 'none' })}}
             >
                 <MenuIcon />
             </IconButton>
@@ -64,7 +88,10 @@ const Header = () => {
           </Grid>
           <Grid item xs={3} align="right">
             <IconButton>
-              <SearchIcon />
+              <ExpandCircle sx={{ fontSize: "30px" }}/>
+            </ IconButton>
+            <IconButton>
+              <SearchIcon sx={{ fontSize: "30px" }}/>
             </IconButton>
           </Grid>
         </Grid>
@@ -76,22 +103,54 @@ const Header = () => {
           '& .MuiDrawer-paper': {
             width: drawerWidth,
             boxSizing: 'border-box',
-          },
+          }, bgcolor: 'blue'
         }}
-        variant="persistent" anchor="left"open={open}
+        variant="persistent" anchor="left"open={openDR}
       >
         <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
+              <ChevronLeftIcon sx={{ fontSize: "30px" }}/>
           </IconButton>
         </DrawerHeader>
-        </Drawer>
+        <Divider />
+        <List component="nav" subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+            Secciones
+        </ListSubheader>
+        }>
+          {secciones.map((seccion) => 
+            <div>
+            {(seccion.content.length===0) ? (
+              <ListItemButton>
+                <ListItemText primary={seccion.title} />
+              </ListItemButton>
+            ) :  (
+              <div>
+              <ListItemButton onClick={() => handleClickMenu(seccion.id)}>
+                <ListItemText primary={seccion.title} />
+                {openMenu[seccion.id] ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={openMenu[seccion.id]} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                {secciones[seccion.id].content.map((subseccion) => 
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemText primary={subseccion} />
+                  </ListItemButton>
+                )}
+                </List>
+              </Collapse>
+              </div>
+            )}
+            </div>
+          )}
+        </List>
+      </Drawer>
 
       <Toolbar sx={{justifyContent: 'space-between', mx: 40}}>
-        <Button sx={{color:"blue"}} color="inherit" noWrap> Inicio </Button>
-        <Button sx={{color:"blue"}} color="inherit" noWrap> Mapa Web </Button>
-        <Button sx={{color:"blue"}} color="inherit" noWrap> Contacto </Button>
-        <Button sx={{color:"blue"}} color="inherit" noWrap> Aviso legal </Button>
+        <Button sx={{color:"blue"}} color="inherit"> Inicio </Button>
+        <Button sx={{color:"blue"}} color="inherit"> Mapa Web </Button>
+        <Button sx={{color:"blue"}} color="inherit"> Contacto </Button>
+        <Button sx={{color:"blue"}} color="inherit"> Aviso legal </Button>
       </Toolbar>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const secciones = [
+    {id:0, title: "INICIO", content: Array()},
+    {id:1, title: "AYUNTAMIENTO", content: Array("Corporación Municipal", "Saluda", "Normativa Municipal", "Tablón de Anuncios", "Teléfonos de Interés", "Trámites y Solicitudes", "Eventos", "Noticias")},
+    {id:2, title: "LOCALIZACIÓN", content: Array()},
+    {id:3, title: "MUNICIPIO", content: Array("Datos del Municipio", "Estadísticas", "Introducción Histórica", "Escudo y Bandera", "Galería Fotográfica", "El Tiempo")},
+    {id:4, title: "LOS PUEBLOS", content: Array("Benllera","Carrocera","Cuevas de Viñayo","Otero de las Dueñas","Piedrasecha","Santiago de las Villas","Viñayo")},
+    {id:5, title: "TURISMO Y OCIO", content: Array("Alojamientos","Bares y Restaurantes","Artesanía","Espacios Naturales","Ferias, Fiestas y Tradiciones","Patrimonio","Productos Agroalimentarios")},
+    {id:6, title: "PERFIL DEL CONTRATANTE", content: Array()},
+    {id:7, title: "PAGO A PROVEEDORES", content: Array()},
+    {id:8, title: "PORTAL DE TRANSPARENCIA", content: Array()}
+];
